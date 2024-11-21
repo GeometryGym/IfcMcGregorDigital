71230b2fd2eaa95bd60e254aca217ae996c2ca2e
Jon Mirtschin <jonm@geometrygym.com>
2024-11-22 06:32:15 +1100

    ReviseIFC

---
+++ ReviseIFC/ReviseIFC/Program.cs
@@ -0,0 +1,32 @@
+﻿using GeometryGym.Ifc;
+using System.Windows;
+using System.Windows.Forms;
+
+public class ReviseIFC
+{
+	[STAThread]
+	static void Main(string[] args)
+	{
+		// 
+		OpenFileDialog openFileDialog = new OpenFileDialog();
+		openFileDialog.Filter = "IFC (*.ifc)|*.ifc";
+		if (openFileDialog.ShowDialog() != DialogResult.OK)
+			return;
+
+		DatabaseIfc db = new DatabaseIfc(openFileDialog.FileName);
+		foreach (IfcProduct product in db.OfType<IfcProduct>())
+		{
+			IfcPropertySingleValue psv = product.FindProperty("MANIFEST", true) as IfcPropertySingleValue;
+			if(psv == null)
+				psv = product.FindProperty("Manifest", true) as IfcPropertySingleValue;
+			if (psv != null && psv.NominalValue != null)
+				product.Name = psv.NominalValue.ValueString;
+		}
+		SaveFileDialog saveFileDialog = new SaveFileDialog();
+		saveFileDialog.Filter = openFileDialog.Filter;
+		if (saveFileDialog.ShowDialog() != DialogResult.OK)
+			return;
+		db.WriteFile(saveFileDialog.FileName);
+	}
+}
+
